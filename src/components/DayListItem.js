4bc40e23f2fbebf867c61261c18416d7ca4817c3
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-31 17:48:55 -0600

    add working DayListItem.js and tests, add empty DayList.js

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,12 +4,20 @@ import classNames from "classnames";
 import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
-
+  const dayClass = "day-list__item" + classNames({
+    "--selected": props.selected,
+    "--full": (props.spots === 0)
+  })
+  const { spots } = props;
   const setDayClick = () => props.setDay(props.name);
   return (
-    <li onClick={setDayClick}>
+    <li onClick={setDayClick} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {!spots && <h3 className="text--light">no spots remaining</h3>}
+      {spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
+      {spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
     </li>
   );
 }
+
+
