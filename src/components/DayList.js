9d83deb70b93fb845db90c1a05e04c62fca5bea0
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-31 18:36:09 -0600

    add working DayList.js, setup Application.js to use useState

---
+++ src/components/DayList.js
@@ -0,0 +1,24 @@
+import DayListItem from "./DayListItem";
+import React from "react";
+
+
+
+const DayList = function(props) {
+  const days = props.days.map(day => {
+     return <DayListItem
+    key={day.id}
+    name={day.name}
+    spots={day.spots}
+    selected={day.name === props.day}
+    setDay={props.setDay} />
+  })
+  return (
+    <ul>
+      {days}
+    </ul>
+  )
+}
+
+
+
+export default DayList;
