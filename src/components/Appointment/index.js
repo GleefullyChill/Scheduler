ed45c753f341f3a091d50f5254c99acc0143fb95
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-01 21:51:16 -0600

    start Show.js and associated stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,14 +5,21 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Empty"
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 const Appointment = function(props) {
+  const { student, interviewer }
   const onAdd = function () {};
+  const onEdit = function () {};
+  const onDelete = function () {};
+
   return (
     <article className="appointment">
       <Header time=""/>
       <Empty onAdd={onAdd} />
+      <Show student={student} interviewer={interviewer} onDelete={onDelete} onEdit={onEdit} />
+
     </article>
   );
 }
