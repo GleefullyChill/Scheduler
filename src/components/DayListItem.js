b11aa9c2b53cd735182ab1dc52d573822da2ce57
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-01 19:28:38 -0600

    add functional InterviewListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,16 @@ import classNames from "classnames";
 import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
+  const { spots, name, setDay, selected } = props;
   const dayClass = "day-list__item" + classNames({
-    "--selected": props.selected,
-    "--full": (props.spots === 0)
+    "--selected": selected,
+    "--full": (spots === 0)
   })
-  const { spots } = props;
-  const setDayClick = () => props.setDay(props.name);
+  
+  const setDayClick = () => setDay(name);
   return (
     <li onClick={setDayClick} className={dayClass}>
-      <h2 className="text--regular">{props.name}</h2> 
+      <h2 className="text--regular">{name}</h2> 
       {!spots && <h3 className="text--light">no spots remaining</h3>}
       {spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
       {spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
---
+++ src/components/InterviewListItem.js
@@ -0,0 +1,32 @@
+
+
+
+import React from "react";
+
+import "components/InterviewListItem.scss"
+
+const InterviewerListItem = function(props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+  const setInterviewerClick = () => setInterviewer(id);
+  return (
+    <>
+      {!selected && <li className="interviewers__item" id={id} onClick={setInterviewerClick}>
+        <img
+          className="interviewers__item-image"
+          src={avatar}
+          alt={name}
+        />
+      </li>}
+      {selected && <li className="interviewers__item--selected" id={id} onClick={setInterviewerClick}>
+        <img
+          className="interviewers__item-image"
+          src={avatar}
+          alt={name}
+        />
+        {name}
+      </li>}
+    </>
+  )
+}
+
+export default InterviewerListItem;
