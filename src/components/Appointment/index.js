5f67559aeffa503fe2005bc7e052aa4c25f9c28b
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-04 11:02:04 -0600

    reformat Appointment to use modes, instead of only conditional statements

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,17 +6,45 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 import "components/Appointment/styles.scss";
 
+
+
+
 const Appointment = function(props) {
-  const { time, interview, onAdd, onEdit, onDelete } = props;
- 
+  const { time, interview, interviewers, onSave } = props;
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
       <Header time={ time } />
-      { interview ? <Show student={interview.student} interviewer={interview.interviewer} onDelete={onDelete} onEdit={onEdit} /> : <Empty onAdd={onAdd} /> }
+      {mode === EMPTY &&
+      <Empty
+      onAdd={() => {
+        transition(CREATE)}
+      }
+      />}
+      { mode === SHOW &&
+      <Show
+      student={interview.student}
+      interviewer={interview.interviewer}
+      onDelete={() => console.log(`Clicked onDelete`)}
+      onEdit={() => console.log(`Clicked onEdit`)}
+      />}
+      {mode === CREATE &&
+      <Form interviewers={[]}
+      onSave={onSave}
+      onCancel={() => {
+        back();
+      }} />}
     </article>
   );
 }
