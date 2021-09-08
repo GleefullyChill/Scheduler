860c287570847542ee70c5c14718ed4742fd9ed6
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-08 13:22:13 -0600

    add more relevant test script in Appointment.test.js, create Form.test.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -10,7 +10,7 @@ import "components/Appointment/styles.scss";
 
 const Form = function(props) {
   const { interviewers, onSave, onCancel } = props;
-  const [interviewer, setInterviewer] = useState(props.interviewer || 0);
+  const [interviewer, setInterviewer] = useState(props.interviewer || 1);
   const [name, setName] = useState(props.name || "");
 
   const reset = function() {
@@ -29,9 +29,11 @@ const Form = function(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
+
         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
+            data-testid="student-name-input"
             name={props.name}
             value={name}
             type="text"
@@ -40,16 +42,15 @@ const Form = function(props) {
               event.preventDefault()
               setName(event.target.value);
             }}
-            /*
-              This must be a controlled component
-            */
           />
         </form>
+
         <InterviewerList
         interviewers={interviewers}
         interviewer={interviewer}
         onChange={setInterviewer} />
       </section>
+
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 
 
 
+import PropTypes from "prop-types";
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss"
